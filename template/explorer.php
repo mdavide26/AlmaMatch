9ab16c2cacb44ec2f97535ab9ff591777ced00a6
casadioalex <casadioalex04@gmail.com>
2025-11-28 00:30:46 +0100

    finita grafica explorer

--- template/explorer.php
+++ template/explorer.php
@@ -2,70 +2,37 @@
 
         <button class="card biomedic" role="listitem" tabindex="0" data-key="biomedic">
           <div class="top">
-            <div>
-              <div class="label">Biomedic</div>
-              <div class="title">Scienze Biomediche</div>
-              <div class="subtitle">Biologia • Chimica</div>
+            <div class="image-container">
+              <img src="../upload/uploads/Biology.png" class="img-responsive" alt="Icona rapresentante la biologia con 2 ampolle collegate da un tubicino ed una fogliara volante tra loro.">
             </div>
-            <div class="icon" aria-hidden>
-              <!-- microscope SVG -->
-              <svg width="48" height="48" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M10 3h4v2h-4z" fill="#081426"/>
-                <path d="M6 19h12v2H6z" fill="#081426"/>
-                <path d="M9 7h6v2H9z" fill="#081426"/>
-              </svg>
-            </div>
+            <div class="image-caption">Biology</div>
           </div>
         </button>
 
         <button class="card electronic" role="listitem" tabindex="0" data-key="electronic">
           <div class="top">
-            <div>
-              <div class="label">Electronic</div>
-              <div class="title">Ingegneria Elettronica</div>
-              <div class="subtitle">Elettronica • Energia</div>
+            <div class="image-container">
+              <img src="../upload/uploads/Electronic.png" class="img-responsive" alt="Icona rapresentante l'elettronica con un circuito composto da una lampadina ,una resistenza ,un condensatore ed una batteria collegate tra loro.">
             </div>
-            <div class="icon" aria-hidden>
-              <!-- bulb svg -->
-              <svg width="48" height="48" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <circle cx="12" cy="9" r="4" fill="#081426" />
-                <rect x="10" y="13" width="4" height="4" fill="#081426" rx="1"/>
-              </svg>
-            </div>
+            <div class="image-caption">Electronic</div>
           </div>
         </button>
 
         <button class="card mechanic" role="listitem" tabindex="0" data-key="mechanic">
           <div class="top">
-            <div>
-              <div class="label">Mechanic</div>
-              <div class="title">Ingegneria Meccanica</div>
-              <div class="subtitle">Robotica • Design</div>
+            <div class="image-container">
+              <img src="../upload/uploads/Mechanic.png" class="img-responsive" alt="Icona rapresentante la meccanica con un braccio meccanico giallo.">
             </div>
-            <div class="icon" aria-hidden>
-              <!-- robot arm svg -->
-              <svg width="48" height="48" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <rect x="3" y="3" width="18" height="6" rx="2" fill="#081426"/>
-                <path d="M6 13h12v6H6z" fill="#081426"/>
-              </svg>
-            </div>
+            <div class="image-caption">Mechanic</div>
           </div>
         </button>
 
         <button class="card compsci" role="listitem" tabindex="0" data-key="compsci">
           <div class="top">
-            <div>
-              <div class="label">Computer Science</div>
-              <div class="title">Informatica</div>
-              <div class="subtitle">Software • AI</div>
+            <div class="image-container">
+              <img src="../upload/uploads/ComputerScience.png" class="img-responsive" alt="Icona rapresentante l'informatica con un uomo incapucciato e con occhiali da sole e giacca blu che scrive su un computer portatile.">
             </div>
-            <div class="icon" aria-hidden>
-              <!-- laptop svg -->
-              <svg width="48" height="48" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <rect x="3" y="5" width="18" height="12" rx="2" fill="#081426"/>
-                <rect x="7" y="17" width="10" height="2" rx="1" fill="#081426"/>
-              </svg>
-            </div>
+            <div class="image-caption">Computer Science</div>
           </div>
         </button>
         
