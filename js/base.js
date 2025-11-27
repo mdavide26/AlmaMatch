b2f9cc0b2e51ab33aa111c3f759ed68010236e75
Davide Magyari <davideandrea.magyari@studio.unibo.it>
2025-11-27 15:24:50 +0100

    Added bootstrap

---
+++ js/base.js
@@ -0,0 +1,8 @@
+const setTheme = () => {
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    document.documentElement.setAttribute('data-bs-theme', prefersDark ? 'dark' : 'light');
+};
+
+setTheme();
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', setTheme)
--- template/base.php
+++ template/base.php
@@ -3,9 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/base.css">
     <title>AlmaMatch</title>
+    <script src="js/base.js"></script>
 </head>
-<body>
+<body class="container-fluid">
     <h1>💖 Welcome to AlmaMatch 💖 </h1>
+    
+    <!-- Content visible for Desktop -->
+    <div class="d-none d-md-block">
+    <header>
+        <h2>Alma</h2><h2>Match</h2>
+    </header>
+    </div>
+
+    <!-- Content visible for Mobile -->
+    <div class="d-block d-md-none">
+    <h2>Mobile Content</h2>
+    <p>Il tema di questa pagina (chiaro o scuro) dipende dalle impostazioni del tuo dispositivo.</p>
+    <button type="button" class="btn adaptive-button">Bottone Adattivo</button>
+    </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
