af48934bf12afd5a7362d2016ac69c3e8d797e31
Davide Magyari <davideandrea.magyari@studio.unibo.it>
2025-12-01 21:48:14 +0100

    minor fixes

--- template/explorer.php
+++ template/explorer.php
@@ -3,7 +3,7 @@
   <button class="card biomedic" role="listitem" tabindex="0" data-key="biomedic">
     <div class="top">
       <div class="image-container">
-        <img src="../upload/uploads/Biology.png" class="img-responsive" alt="Icona rapresentante la biologia con 2 ampolle collegate da un tubicino ed una fogliara volante tra loro.">
+        <img src="../upload/explorer-categories/Biology.png" class="img-responsive" alt="Icona rapresentante la biologia con 2 ampolle collegate da un tubicino ed una fogliara volante tra loro.">
       </div>
       <div class="image-caption">Biology</div>
     </div>
@@ -12,7 +12,7 @@
   <button class="card electronic" role="listitem" tabindex="0" data-key="electronic">
     <div class="top">
       <div class="image-container">
-        <img src="../upload/uploads/Electronic.png" class="img-responsive" alt="Icona rapresentante l'elettronica con un circuito composto da una lampadina ,una resistenza ,un condensatore ed una batteria collegate tra loro.">
+        <img src="../upload/explorer-categories/Electronic.png" class="img-responsive" alt="Icona rapresentante l'elettronica con un circuito composto da una lampadina ,una resistenza ,un condensatore ed una batteria collegate tra loro.">
       </div>
       <div class="image-caption">Electronic</div>
     </div>
@@ -21,7 +21,7 @@
   <button class="card mechanic" role="listitem" tabindex="0" data-key="mechanic">
     <div class="top">
       <div class="image-container">
-        <img src="../upload/uploads/Mechanic.png" class="img-responsive" alt="Icona rapresentante la meccanica con un braccio meccanico giallo.">
+        <img src="../upload/explorer-categories/Mechanic.png" class="img-responsive" alt="Icona rapresentante la meccanica con un braccio meccanico giallo.">
       </div>
       <div class="image-caption">Mechanic</div>
     </div>
@@ -30,7 +30,7 @@
   <button class="card compsci" role="listitem" tabindex="0" data-key="compsci">
     <div class="top">
       <div class="image-container">
-        <img src="../upload/uploads/ComputerScience.png" class="img-responsive" alt="Icona rapresentante l'informatica con un uomo incapucciato e con occhiali da sole e giacca blu che scrive su un computer portatile.">
+        <img src="../upload/explorer-categories/ComputerScience.png" class="img-responsive" alt="Icona rapresentante l'informatica con un uomo incapucciato e con occhiali da sole e giacca blu che scrive su un computer portatile.">
       </div>
       <div class="image-caption">Computer Science</div>
     </div>
