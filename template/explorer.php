eefee70feacbda57680a79fa84c3c5e6b0ab20dd
casadioalex <casadioalex04@gmail.com>
2025-12-03 04:44:59 +0100

    centerd captions in explorer

--- template/explorer.php
+++ template/explorer.php
@@ -1,39 +1,38 @@
-<section class="grid" role="list" aria-label="Ambiti universitari">
-
-  <button class="card biomedic" role="listitem" tabindex="0" data-key="biomedic">
-    <div class="top">
-      <div class="image-container">
-        <img src="../upload/uploads/Biology.png" class="img-responsive" alt="Icona rapresentante la biologia con 2 ampolle collegate da un tubicino ed una fogliara volante tra loro.">
-      </div>
-      <div class="image-caption">Biology</div>
+<div class="almamatch-container">
+    <div class="container">
+        <div class="row row-cols-2 g-3" role="list" aria-label="Ambiti universitari">
+            <div class="col">
+                <button class="w-100 position-relative card biomedic" role="listitem" tabindex="0" data-key="biomedic">
+                    <figure>
+                      <img class="card-img h-100 w-100" src="../upload/uploads/Biology.png" alt="Icona rappresentante la biologia con 2 ampolle collegate da un tubicino ed una foglia volante tra loro.">
+                      <figcaption class="image-caption" aria-hidden="true">Biology</figcaption>
+                    </figure>
+                </button>
+            </div>
+            <div class="col">
+                <button class="w-100 position-relative card electronic" role="listitem" tabindex="0" data-key="electronic">
+                    <figure>
+                      <img class="card-img h-100 w-100" src="../upload/uploads/Electronic.png" alt="Icona rappresentante l'elettronica con un circuito composto da una lampadina, una resistenza, un condensatore ed una batteria collegate tra loro.">
+                      <figcaption class="image-caption" aria-hidden="true">Electronic</figcaption>
+                    </figure>
+                </button>
+            </div>
+            <div class="col">
+                <button class="w-100 position-relative card mechanic" role="listitem" tabindex="0" data-key="mechanic">
+                    <figure>
+                      <img class="card-img h-100 w-100" src="../upload/uploads/Mechanic.png" alt="Icona rappresentante la meccanica con un braccio meccanico giallo.">
+                      <figcaption class="image-caption" aria-hidden="true">Mechanic</figcaption>
+                    </figure>
+                </button>
+            </div>
+            <div class="col">
+                <button class="w-100 position-relative card compsci" role="listitem" tabindex="0" data-key="compsci">
+                    <figure>
+                      <img class="card-img h-100 w-100" src="../upload/uploads/ComputerScience.png" alt="Icona rappresentante l'informatica con un uomo incappucciato e con occhiali da sole e giacca blu che scrive su un computer portatile.">
+                      <figcaption class="image-caption" aria-hidden="true">Computer Science</figcaption>
+                    </figure>
+                </button>
+            </div>
+        </div>
     </div>
-  </button>
-
-  <button class="card electronic" role="listitem" tabindex="0" data-key="electronic">
-    <div class="top">
-      <div class="image-container">
-        <img src="../upload/uploads/Electronic.png" class="img-responsive" alt="Icona rapresentante l'elettronica con un circuito composto da una lampadina ,una resistenza ,un condensatore ed una batteria collegate tra loro.">
-      </div>
-      <div class="image-caption">Electronic</div>
-    </div>
-  </button>
-
-  <button class="card mechanic" role="listitem" tabindex="0" data-key="mechanic">
-    <div class="top">
-      <div class="image-container">
-        <img src="../upload/uploads/Mechanic.png" class="img-responsive" alt="Icona rapresentante la meccanica con un braccio meccanico giallo.">
-      </div>
-      <div class="image-caption">Mechanic</div>
-    </div>
-  </button>
-
-  <button class="card compsci" role="listitem" tabindex="0" data-key="compsci">
-    <div class="top">
-      <div class="image-container">
-        <img src="../upload/uploads/ComputerScience.png" class="img-responsive" alt="Icona rapresentante l'informatica con un uomo incapucciato e con occhiali da sole e giacca blu che scrive su un computer portatile.">
-      </div>
-      <div class="image-caption">Computer Science</div>
-    </div>
-  </button>
-  
-</section>
+</div>
